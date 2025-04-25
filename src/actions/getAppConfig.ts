2bdd5040d1026f966c8ab4a29629772f06fec4b8
Dillon <179108267+iceorcus@users.noreply.github.com>
2025-04-26 03:17:41 +1000

    updated api endpoint

--- src/actions/getAppConfig.ts
+++ src/actions/getAppConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const getAppConfig = async (clientId: any) => {
-    const response = await axios.get(`http://localhost:3050/api/invoke-oauth/${clientId}`);
+    const response = await axios.get(`https://api.id.endr.tech/api/invoke-oauth/${clientId}`);
 
     if(response.status === 200) {
         return response.data;
